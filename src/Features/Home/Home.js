065db7e829d1e2d0934f329f3e01a7ce083a32be
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-06-11 11:26:05 +0800

    +home.js, home.css, green bordered rectangle

---
+++ src/Features/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="rectangle">
+          <img src="grad.jpg" alt="Italian Trulli"/>
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Features/Home/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
